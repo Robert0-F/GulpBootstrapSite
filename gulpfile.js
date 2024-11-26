1ce91942f497ec043ae2df641df9f561f1adc500
Robert0-F <72812832+Robert0-F@users.noreply.github.com>
2024-11-27 01:03:14 +0300

    Создание минимизатора css и js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,42 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+ minifyCSS = require('gulp-clean-css'),
+ rename = require('gulp-rename'),
+ sass = require('gulp-sass')(require('sass')),
+ minifyJS = require('gulp-minify'),
+ browserSync = require('browser-sync').create();
 
-gulp.task('default', async function(done){
-    console.log('Hellow Project');
-    done();
+gulp.task('minCss', async function(){
+    gulp.src('app/css/*.scss')
+      .pipe(sass())
+      .pipe(minifyCSS())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('public/css'))
+      .pipe(browserSync.stream());
 });
+
+gulp.task('minJs', async function(){
+    gulp.src('app/js/*.js')
+      .pipe(minifyJS())
+      .pipe(gulp.dest('public/js'))
+      .pipe(browserSync.stream());
+    });
+    
+gulp.task('watchAll', async function() {
+    gulp.watch("app/css/*.scss", gulp.series('minCss'))
+    gulp.watch("app/css/*.js", gulp.series('minJs'));
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: "public/"
+    });
+
+
+    gulp.watch("public/*html").on('change', 
+        browserSync.reload);
+});
+
+
+gulp.task('default', gulp.parallel('browserSync', 'watchAll'))
